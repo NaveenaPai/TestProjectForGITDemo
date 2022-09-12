11580d0ab5eecc0db166b1ae6801b15edba14ea2
NaveenaPai <104799368+NaveenaPai@users.noreply.github.com>
2022-09-12 13:53:52 -0400

    Update DemoTest.java

--- src/test/java/demo/tests/DemoTest.java
+++ src/test/java/demo/tests/DemoTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+/* Demo test class created to demonstrate working with different GIT commands */
 public class DemoTest {
 
 	@Test()
