9490ab5726eabead04875280f957fa975386a978
ppai2114 <ppai2114@DESKTOP-DIMGNTL.wks.jnj.com>
2022-09-12 11:41:23 -0400

    Commit newly added files/changes

--- src/test/java/demo/tests/DemoTest.java
+++ src/test/java/demo/tests/DemoTest.java
@@ -1,9 +1,19 @@
 package demo.tests;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class DemoTest {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	@Test()
+	public void TestMethod() {
+
+		String filepath = System.getProperty("user.dir") + "/src/test/java/driver/chromedriver.exe";
+		System.setProperty("webdriver.chrome.driver", filepath);
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		driver.quit();
 
 	}
 
